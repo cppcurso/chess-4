eb89a45c85ef8fcf239770977c79bd8a66e79a9b
Antonio <pepi@cev.com>
2018-02-28 14:29:19 +0100

    to movement

--- main.cpp
+++ main.cpp
@@ -3,9 +3,34 @@
 
 //_____CHESS_____//
 
-int main()
+ void newPlay(bool black,)
+ {
+
+   if (black == false) {cout<< "----------WHITE TURN-----------"<<endl;}
+   else {cout<< "----------BLACK TURN-----------"<<endl;}
+   cout<<" X OF THE FIGURE TO MOVE"<<endl;
+   //ask_x();
+   cout<<" Y OF THE FIGURE TO MOVE"<<endl;
+   //ask_Y();
+ }
+int ask_X()
 {
+  int x;
+  cin>>x;
+  return x;
+}
+int ask_Y()
+{
+  int y;
+  cin>>y;
+  return y;
+}
+
+}
+int main(){
   Board board;
   //board.newGame();
 board.newGame();
+
+
 }
