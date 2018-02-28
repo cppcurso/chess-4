0d45e4fdfdafe978d5d051070d42a2b90882c518
CarlosSP <Carlos@cev.com>
2018-02-28 10:09:27 +0100

    added king and queen

---
+++ King.h
@@ -0,0 +1,38 @@
+/*
+ * King.h
+ *
+ *  Created on: 28/02/2018
+ *      Author: usuario
+ */
+
+#include "Piece.h"
+#ifndef KING_H_
+#define KING_H_
+#include <stdlib.h>
+
+class King : public Piece {
+
+public:
+	bool checkMove(int x, int y) {
+		int a = x - this->x;
+		int b = y - this->y;
+		a = a * a;
+		b = b * b;
+		if (this->y+1==y || this->x+1==x || a+b==2) {
+			return true;
+		} return false;
+	}
+
+	int virtual move (int x, int y) {
+		this->x=x;
+		this->y=y;
+	}
+
+	King() {
+			figure='K';
+			x=0
+			y=3;
+		}
+};
+
+#endif /* KING_H_ */
---
+++ Queen.h
@@ -0,0 +1,38 @@
+/*
+ * Queen.h
+ *
+ *  Created on: 28/02/2018
+ *      Author: usuario
+ */
+
+#include "Piece.h"
+#ifndef QUEEN_H_
+#define QUEEN_H_
+#include <stdlib.h>
+
+class Queen : public Piece {
+
+public:
+	bool checkMove(int x, int y) {
+		int a = x - this->x;
+		int b = y - this->y;
+		a = abs (a);
+		b = abs (b);
+		if (this->y==y || this->x==x || a == b) {
+			return true;
+		} return false;
+	}
+
+	int virtual move (int x, int y) {
+		this->x=x;
+		this->y=y;
+	}
+
+	Queen() {
+			figure='Q';
+			x=0;
+			y=4;
+		}
+};
+
+#endif /* QUEEN_H_ */
